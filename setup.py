8f3ebc7f3932153c3d3957a981aa4b771ece65a4
timmartin19 <tim.martin@vertical-knowledge.com>
2015-02-11 18:59:46 -0500

    fixed packages in setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,15 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
-from setuptools import setup
-from os import path
+from setuptools import setup, find_packages
 
-here = path.abspath(path.dirname(__file__))
 
 version = '0.1.0b1'
 
 setup(
     name='flask-ripozo',
     version=version,
-    packages=[], # TODO
+    packages=find_packages(exclude=['tests', 'tests.*']),
     url='',
     license='',
     author='Tim Martin',
