626ca29789c2652305d1057f72dbd3005794a2ec
timmartin19 <tim.martin@vertical-knowledge.com>
2015-04-10 12:39:42 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -8,18 +8,34 @@ from setuptools import setup, find_packages
 version = '0.1.2.dev0'
 
 setup(
-    name='flask-ripozo',
-    version=version,
-    packages=find_packages(exclude=['tests', 'tests.*']),
-    url='',
-    license='',
     author='Tim Martin',
     author_email='tim.martin@vertical-knowledge.com',
-    description='',
-    install_requires=['ripozo', 'Flask'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: Other/Proprietary License',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    description='An extension for ripozo and that brings HATEOAS/REST/Hypermedia apis to flask',
+    install_requires=[
+        'ripozo',
+        'Flask'
+    ],
+    name='flask-ripozo',
+    packages=find_packages(exclude=['tests', 'tests.*']),
     test_suite='tests',
     tests_require=[
         'ripozo-tests',
         'tox'
-    ]
+    ],
+    url='http://flask-ripozo.readthedocs.org/',
+    version=version
 )
